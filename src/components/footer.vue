61c068da64e405f14d5d933910f28b0f47d7deea
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-07-11 02:05:37 -0300

    fork it

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -7,16 +7,24 @@ export default {
 <template>
   <p class="text-lg">
     Made with
-    <a href="https://vuejs.org" class="no-underline text-white" target="_blank">
+    <a href="https://vuejs.org" class="no-underline" target="_blank">
       <i title="Vue.js" aria-label="Vue.js" class="fab fa-vuejs"></i>
     </a>
     by
-    <a class="no-underline text-white" target="_blank" href="https://github.com/vinicius73">@vinicius73</a>
+    <a class="no-underline" target="_blank" href="https://github.com/vinicius73">@vinicius73</a>
+    <br>
+    <a target="_blank" class="text-sm" href="https://github.com/vinicius73/gravatar-url-generator">
+      fork it <i class="fas fa-code-branch"></i>
+    </a>
   </p>
 </template>
 
 <style scoped>
   p {
     font-family: 'Ubuntu Mono', monospace;
+  }
+  a, p {
+    color: #fff;
+    text-decoration: none;
   }
 </style>
