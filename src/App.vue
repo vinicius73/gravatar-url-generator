1a2da0717d9a52b2d843d7a3e65f323c943bb9bf
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-07-10 21:39:57 -0300

    remove demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    HelloWorld
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  @import "~milligram"
 </style>
