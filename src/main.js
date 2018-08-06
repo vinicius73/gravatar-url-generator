e4c8d50b1e1a6ef3c11a98ea464feb3999187d28
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-08-06 14:10:07 -0300

    performance in development

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import PageTitle from 'vue-page-title'
 import App from './App.vue'
 import router from './router'
+import { isProduction } from './env'
 
 import './registerServiceWorker'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = !isProduction
+Vue.config.performance = !isProduction
 
 Vue.use(PageTitle, {
   suffix: ' - Avatar URL Generator  '
